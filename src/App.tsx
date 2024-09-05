e6bb41199741d0be6cf30daeac804ad729f8d703
Dmytro Nasyrov <dmytro@pharosproduction.com>
2024-09-05 11:29:11 +0300

    add deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import {useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import {TonConnectButton} from "@tonconnect/ui-react";
 import {useMainContract} from "./hooks/useMainContract.ts";
@@ -11,8 +8,8 @@ function App() {
     const {
         contract_address,
         counter_value,
-        recent_sender,
-        owner,
+        // recent_sender,
+        // owner,
         contract_balance,
         sendIncrement,
         sendDeposit,
